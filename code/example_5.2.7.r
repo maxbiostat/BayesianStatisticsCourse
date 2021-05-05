a0cb71b7b9b08dd08ddf3e1e91007e00976ffebe
Luiz Max Carvalho <luizepidemiologia@gmail.com>
2021-05-05 14:14:12 -0300

    updates

---
+++ code/example_5.2.7.r
@@ -0,0 +1,45 @@
+log1pexp <- function(x)
+{
+  ## taken from https://github.com/mfasiolo/qgam/blob/3bff42449b865a12c264c5b61438def5d74fdc70/R/log1pexp.R
+  indx <- .bincode(x, 
+                   c(-Inf, -37, 18, 33.3, Inf), 
+                   right = TRUE, 
+                   include.lowest = TRUE)
+  
+  kk <- which(indx==1)
+  if( length(kk) ){  x[kk] <- exp(x[kk])  }
+  
+  kk <- which(indx==2)
+  if( length(kk) ){  x[kk] <- log1p( exp(x[kk]) ) }
+  
+  kk <- which(indx==3)
+  if( length(kk) ){  x[kk] <- x[kk] + exp(-x[kk]) }
+  
+  return(x)
+}
+####
+
+logPr <- function(x, n, rho0){
+  -log1pexp(
+    log1p(-rho0)-log(rho0) +
+      n * log(2) + 
+      lbeta(x + 1, n -x +1)
+  )
+}
+
+f_rho <- function(rho){
+  exp(logPr(x = 5, n = 10, rho0 = rho))
+}
+f_rho <- Vectorize(f_rho)
+
+curve(f_rho, xlab = expression(rho), ylab = "Posterior probability",
+      main = "x = 5, n = 10")
+
+f_x <- function(x){
+  exp(logPr(x = x, n = 10, rho0 = 1/2))
+}
+f_x <- Vectorize(f_x)
+
+curve(f_x, 0, 10,
+      xlab = expression(rho), ylab = "Posterior probability",
+      main = "x = 5, n = 10")
